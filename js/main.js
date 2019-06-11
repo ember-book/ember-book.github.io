74cef73338d085ff9ad76c9ea058470afa16c852
Matic Jurglic <matic@jurglic.si>
2019-06-11 16:45:20 +0200

    Add affiliate popup

--- js/main.js
+++ js/main.js
@@ -1,176 +1,177 @@
 /* Template: Ethority | Author: GD Themes | Version: 1.0.0  */
 /*----------------------------------------------------------*/
 (function($) {
+  "use strict";
 
-"use strict";
+  $(document).ready(function() {
+    $("#affiliateModal").modal({
+      fadeDuration: 100
+    });
 
-$(document).ready(function() {
+    $(".email-button").click(function(e) {
+      e.preventDefault();
+      var email = $(".email-input").val();
 
-	$('.email-button').click(function(e) {
-		e.preventDefault()
-		var email = $('.email-input').val();
+      if (String(email) === "") {
+        s;
+        $(".email-input").focus();
+        return;
+      }
 
-		if (String(email) === "") {
-			$('.email-input').focus();
-			return;
-		}
+      var url =
+        "https://emberjs-email-sender.herokuapp.com/handle_email?email=" +
+        email +
+        "&from_site=emberjs-book";
+      $(".email-button").text("Sending...");
+      $.post(url, function() {
+        $(".email-button").text("Sent! Check your inbox.");
+      });
+    });
 
-		var url = "https://emberjs-email-sender.herokuapp.com/handle_email?email=" + email + "&from_site=emberjs-book";
-		$('.email-button').text("Sending...");
-		$.post(url, function() {
-			$('.email-button').text("Sent! Check your inbox.");
-		});
-	});
+    // Input placeholder fallback
+    $("input, textarea").placeholder();
 
-	// Input placeholder fallback
-	$('input, textarea').placeholder();
+    $(".flexslider").flexslider({
+      selector: ".slides > .review",
+      directionNav: false,
+      slideshow: true,
+      slideshowSpeed: 5000,
+      animationLoop: true,
+      animation: "slide",
+      direction: "horizontal"
+    });
 
-
-	$('.flexslider').flexslider({
-		selector: ".slides > .review",
-		directionNav: false,
-		slideshow: true,
-		slideshowSpeed: 5000,
-		animationLoop: true,
-		animation: "slide",
-		direction: "horizontal"
-	});
-
-	/* Header
+    /* Header
 	------------------*/
-	var topMenu = $('#menu'),
-			topMenuHeight = topMenu.outerHeight(), // include padding&margin, if .outerHeight(true), only padding
-			menuItems = topMenu.find('a[href*="#"]'),
+    var topMenu = $("#menu"),
+      topMenuHeight = topMenu.outerHeight(), // include padding&margin, if .outerHeight(true), only padding
+      menuItems = topMenu.find('a[href*="#"]'),
+      scrolledItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) {
+          return item;
+        }
+      });
 
-			scrolledItems = menuItems.map( function() {
-				var item = $( $(this).attr('href') );
-				if ( item.length ) { return item; }
-			});
+    // Scroll to section
+    menuItems.click(function(e) {
+      var hrefVal = $(this).attr("href"),
+        offsetTop =
+          hrefVal === "#" ? 0 : $(hrefVal).offset().top - topMenu.outerHeight();
 
+      // mobile
+      var w = $(window).width();
+      if (w < 768) {
+        offsetTop = hrefVal === "#" ? 0 : $(hrefVal).offset().top - 60;
+      }
 
-	// Scroll to section
-	menuItems.click( function( e ) {
+      $("html, body")
+        .stop()
+        .animate({ scrollTop: offsetTop }, 800);
 
-		var hrefVal = $(this).attr('href'),
-				offsetTop = hrefVal === '#' ? 0 : $(hrefVal).offset().top - topMenu.outerHeight();
+      // close mobile menu when item is clicked
+      if ($(window).width() < 768) {
+        $("#menu-toggle").toggleClass("open");
+        $("#menu").slideToggle(400);
+      }
+    }); // menuItems.click( function( e )
 
-		// mobile
-		var w = $(window).width();
-		if ( w < 768 ){
-			offsetTop = hrefVal === '#' ? 0 : $(hrefVal).offset().top - 60;
-		}
+    // Scroll to 'purchase' section
+    $(".purchase-button").click(function(e) {
+      var hrefVal = $(this).attr("href"),
+        hash = hrefVal.substr(hrefVal.indexOf("#")),
+        offsetTop = $(hrefVal).offset().top - topMenu.outerHeight();
 
-		$('html, body').stop().animate({ scrollTop: offsetTop }, 800);
+      if (hrefVal.indexOf("#") === 0) {
+        e.preventDefault();
+      }
 
-		// close mobile menu when item is clicked
-		if ( $(window).width() < 768 ) {
-			$('#menu-toggle').toggleClass('open');
-			$('#menu').slideToggle(400);
-		}
+      $("html, body")
+        .stop()
+        .animate({ scrollTop: offsetTop }, 800);
+    });
 
-	}); // menuItems.click( function( e )
+    $(window).scroll(function() {
+      // Change .active menu item on scroll
+      var fromTop = $(this).scrollTop() + topMenuHeight,
+        // return menu item as object when scrolled into view
+        itemsScrolled = scrolledItems.map(function() {
+          if ($(this).offset().top - 1 < fromTop)
+            // -1 so that menuItem detects using click event
+            return this;
+        });
 
-	// Scroll to 'purchase' section
-	$('.purchase-button').click( function( e ) {
-		var hrefVal = $(this).attr('href'),
-				hash = hrefVal.substr(hrefVal.indexOf("#")),
-				offsetTop = $(hrefVal).offset().top - topMenu.outerHeight();
+      var scrolledItem = itemsScrolled[itemsScrolled.length - 1]; // index starts from 0...
 
+      var id = scrolledItem && scrolledItem.length ? scrolledItem[0].id : "";
 
-		if ( hrefVal.indexOf('#') === 0 ) {
-			e.preventDefault();
-		}
+      menuItems
+        .parent()
+        .removeClass("active")
+        .end()
+        .filter('[href="#' + id + '"]')
+        .parent()
+        .addClass("active");
 
+      // Scale down header
+      if ($(this).scrollTop() < 500) {
+        $("#header").removeClass("scaled-down");
+      } else {
+        $("#header").addClass("scaled-down");
+      }
 
-		$('html, body').stop().animate({ scrollTop: offsetTop }, 800);
-	});
+      if ($(window).scrollTop() < 1000) {
+        $("#back-to-top").fadeOut();
+      } else {
+        $("#back-to-top").fadeIn();
+      }
+    }); // $(window).scroll( function()
 
+    // Mobile Menu Toggle
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $(this).toggleClass("open");
+      $("#menu").slideToggle(400);
+    });
 
-	$(window).scroll( function() {
-
-		// Change .active menu item on scroll
-		var fromTop = $(this).scrollTop() + topMenuHeight,
-
-				// return menu item as object when scrolled into view
-				itemsScrolled = scrolledItems.map( function(){
-					if ( $(this).offset().top - 1 < fromTop ) // -1 so that menuItem detects using click event
-					return this;
-				});
-
-		var scrolledItem = itemsScrolled[itemsScrolled.length - 1]; // index starts from 0...
-
-		var id = scrolledItem && scrolledItem.length ? scrolledItem[0].id : '';
-
-		menuItems
-			.parent().removeClass('active')
-			.end()
-			.filter('[href="#' + id + '"]').parent().addClass('active');
-
-
-		// Scale down header
-		if ( $(this).scrollTop() < 500 ) {
-			$('#header').removeClass('scaled-down');
-		} else {
-			$('#header').addClass('scaled-down');
-		}
-
-		if ( $(window).scrollTop() < 1000 ) {
-			$('#back-to-top').fadeOut();
-		} else {
-			$('#back-to-top').fadeIn();
-		}
-	}); // $(window).scroll( function()
-
-
-	// Mobile Menu Toggle
-	$('#menu-toggle').click( function( e ) {
-		e.preventDefault();
-		$(this).toggleClass('open');
-		$('#menu').slideToggle(400);
-	});
-
-	/*------------------
+    /*------------------
 	End Header */
 
-
-	/* Footer
+    /* Footer
 	------------------*/
-	// back to top
-	$('#back-to-top').click( function( e ) {
-		e.preventDefault();
+    // back to top
+    $("#back-to-top").click(function(e) {
+      e.preventDefault();
 
-		$('html, body').animate({ scrollTop: 0 }, 800);
-	});
+      $("html, body").animate({ scrollTop: 0 }, 800);
+    });
 
-
-	/* Contact Form
+    /* Contact Form
 	------------------*/
-	$('#contact-form').validate({
-	rules: {
-		name: {
-			required: true,
-			minlength: 2
-		},
-		email: {
-			required: true,
-			email: true
-		},
-		message: {
-			required: true,
-			minlength: 10
-		}
-	},
-	messages: {
-		name: "* Please enter in your name.",
-		email: "* Please enter a valid email address.",
-		message: "* Please enter your message."
-	},
-	errorElement: "div",
-	errorPlacement: function(error, element) {
-		element.before(error);
-	}
-});
-
-
-});
-}(jQuery));
+    $("#contact-form").validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        message: {
+          required: true,
+          minlength: 10
+        }
+      },
+      messages: {
+        name: "* Please enter in your name.",
+        email: "* Please enter a valid email address.",
+        message: "* Please enter your message."
+      },
+      errorElement: "div",
+      errorPlacement: function(error, element) {
+        element.before(error);
+      }
+    });
+  });
+})(jQuery);
